c1d7bd39330dd345a964daef2c1f6c4889205725
SainkaTyan <sainka.tyan@gmail.com>
2018-11-19 18:34:09 +0300

    fix

---
+++ Assets/Scripts/Orientation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientation : MonoBehaviour
+{
+
+    void Start()
+    {
+        if (Screen.currentResolution.height < 1920)
+        {
+            Screen.SetResolution(Screen.height / 16 * 9, Screen.height, true);
+        }
+        else
+        {
+            Screen.SetResolution(2160, 3840, true);
+        }
+    }
+}
